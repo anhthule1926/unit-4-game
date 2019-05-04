09b64f2ada0865eec9395a210dd65eb8127cf8e4
Thu Le <anhthu.le1926@gmail.com>
2019-05-04 01:13:52 -0500

    updating index

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,35 @@
 $(document).ready(function(){//I need jQuery to run!
  
 	var redNumber = Math.floor(Math.random()*12);
-	var purpleNumber = Math.floor(Math.random()*12);
+	var yellowNumber = Math.floor(Math.random()*12);
 	var blueNumber = Math.floor(Math.random()*12);
 	var greenNumber = Math.floor(Math.random()*12);
 
 	$("#button-red").attr('value', redNumber);
 	$("#button-blue").attr('value', blueNumber);
+	$("#button-green").attr('value', greenNumber);
+	$("#button-yellow").attr('value', yellowNumber);
 
 	$('.crystal-button').click(function(){
 
-		console.log($(this).attr('value'));
+		console.log($(this).attr('value'));4n
+
+    var userTotal= 0; 
+    var wins= 0;
+    var losses = 0;
+
+    for(var i = 0; 1 < 4; i++){
+        var crystal = $("<div>");
+
+        $(".cystals").crystals();
+        console.log(Crystal)
+    }
+
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+    $("#total").html(totalScore);
 
 	});
 
 });
+
