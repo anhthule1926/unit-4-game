0fda3a5294ccc82798cac0e127bf730ac5e2dc07
Thu Le <anhthu.le1926@gmail.com>
2019-05-05 10:36:19 -0500

    update jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,72 @@
-$(document).ready(function(){//I need jQuery to run!
-	var totalScore, targetScore, redNumber, yellowNumber, blueNumber, greenNumber;
-    var targetScore;
-    var totalScore = 0;
-    var wims = 0;
-    var losses = 0;
-    var redNumber;
-    var yellowNumber;
-    var blueNumber;
-    var greenNumber;
+$(document).ready(function(){
+var totalScore, targetScore, redNumber, yellowNumber, blueNumber, greenNumber;
+var targetScore;
+var totalScore = 0;
+var wins = 0;
+var losses = 0;
+var redNumber;
+var yellowNumber;
+var greenNumber;
+var blueNumber;
 
 
-	var resetGame = function(){
-		targetScore = Math.floor(Math.random()*101 + 19);
-		totalScore = 0;
-		redNumber = Math.floor(Math.random()*12);
-		yellowNumber = Math.floor(Math.random()*12);
-		blueNumber = Math.floor(Math.random()*12);
-		greenNumber = Math.floor(Math.random()*12);
+
+	function newNumbers() {
+		magicNumber = Math.floor(Math.random() * 101 + 19);
+		redNumber = Math.floor(Math.random() * 12);
+		yellowNumber = Math.floor(Math.random() * 12);
+		greenNumber = Math.floor(Math.random() * 12);
+		blueNumber = Math.floor(Math.random() * 12);
 	}
 
-	resetGame();
- 
-	
+	function newGame() {
+		newNumbers();
+		totalScore = 0;
+		$("#targetScore").text(targetScore);
+		$("#totalScore").text(totalScore);
+		$("#redNumber").attr("value", redNumber);
+		$("#yellowNumber").attr("value", yellowNumber);
+		$("#greenNumber").attr("value", greenNumber);
+		$("#blueNumber").attr("value", blueNumber);
+		$("#wins").text(wins);
+		$("#losses").text(losses);
+		$("#winOrLose").text("");
+    }
 
-	$("#button-red").attr('value', redNumber);
-	$("#button-yellow").attr('value', yellowNumber);
-	$("#button-blue").attr('value', blueNumber);
-	$("#button-green").attr('value', greenNumber);
+	function youWin() {
+		$("#winOrLose").text("YOU WIN!");
+		wins++;
+		$("#wins").text(wins);
+	}
 
+	function youLose() {
+		$("#winOrLose").text("YOU LOSE");
+		losses++;
+		$("#losses").text(losses);
+	}
 
-	$('.crystal-button').click(function(){
-		current += parseInt($(this).attr('value'));
-		console.log(current);
+	newGame();
+
+	$(".crystalimg").on("click", function() {
+		if (totalScore >= targetScore) {
+			return;
+		}
+
+		var value = $(this).attr("value");
+		value = parseInt(value);
+		totalScore += value;
+		$("#totalScore").text(totalScore);
+
+		if (totalScore === targetScore) {
+			youWin();
+		} else if (totalScore > targetScore) {
+			youLose();
+		}
 	});
 
-
+	$(".btn").on("click", function() {
+        newGame();
+    
+	});
 
 });
